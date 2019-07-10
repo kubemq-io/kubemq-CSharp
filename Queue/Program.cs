9c69fc65ea011f1519f02fdc6354671a9bf18060
Ido Sheffer <Ido.Sheffer@tradency.com>
2019-07-10 17:49:24 +0300

    added queue exmple

---
+++ Queue/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            KubeMQ.SDK.csharp.Queue.Queue queue = new KubeMQ.SDK.csharp.Queue.Queue("ido", "test");
+           var res = queue.SendQueueMessage(new KubeMQ.SDK.csharp.Queue.Message
+            {
+                MessageID = "123",
+                Body = KubeMQ.SDK.csharp.Tools.Converter.ToByteArray("hi my name is dodo"),
+                Metadata = "MeaAleha",
+                //Tags = 
+
+
+
+            });
+        }
+    }
+}
