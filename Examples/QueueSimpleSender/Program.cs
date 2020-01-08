55584704c864f5edd3965447286ca8f5648d2e4e
eitam-ring <eitam.ring@kubemq.io>
2020-01-08 09:45:12 +0000

    Update Examples

--- Examples/QueueSimpleSender/Program.cs
+++ Examples/QueueSimpleSender/Program.cs
@@ -148,7 +148,7 @@ namespace QueueSimpleSender
             try
             {
                 var q = new KubeMQ.SDK.csharp.Queue.Queue(queueName, ClientID, KubeMQServerAddress);
-                var res = q.AckAllQueueMessagesResponse();
+                var res = q.AckAllQueueMessages();
                 if (res.IsError)
                 {
                     Console.WriteLine($"[DemoSender][acallkmsgs]message dequeue error, error:{res.Error}");
@@ -169,7 +169,7 @@ namespace QueueSimpleSender
             try
             {
                 var q = new KubeMQ.SDK.csharp.Queue.Queue(queueName, ClientID, KubeMQServerAddress);
-                var res = q.PeakQueueMessage();
+                var res = q.PeekQueueMessage();
                 if (res.IsError)
                 {
                     Console.WriteLine($"[DemoSender][peakmsgs]message dequeue error, error:{res.Error}");
