9ae384c2ca722ae0ac43f0335809cf5e2b87e83c
KubeMQ <lior.nabat@kubemq.io>
2021-04-19 12:21:06 +0300

    add new sdk

---
+++ KubeMQ/Events/EventsMessage.cs
@@ -0,0 +1,25 @@
+﻿using KubeMQ.Client;
+using KubeMQ.Grpc;
+using KubeMQ.Utils;
+
+namespace KubeMQ.Events
+{
+    public class EventsMessage :BaseMessage
+    {
+        public Event ToEvent ()
+        {
+            return  new Event
+            {
+                EventID = this.Id,
+                ClientID = this.ClientId,
+                Channel = this.Channel,
+                Metadata = this.Metadata,
+                Body = Convertors.FromByteArray(this.Body),
+                Store = false,
+                Tags = { Convertors.ToMapFields(this.Tags)}
+            };
+
+        }
+    }
+    
+}
