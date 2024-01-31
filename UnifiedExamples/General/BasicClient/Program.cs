486b45f407b9ef72cea00b50f80529b2e78d8360
Lior Nabat <lior.nabat@gmail.com>
2024-01-31 16:21:40 +0200

    Add new client examples for KubeMQ SDK
    This commit adds various example programs to demonstrate different aspects of the KubeMQ SDK. These include a BasicClient, a PubSub Events program, a TlsClient and an AuthClient. Each of these new programs include their own project file and code illustrating specific SDK functionality.

---
+++ UnifiedExamples/General/BasicClient/Program.cs
@@ -0,0 +1,30 @@
+﻿using KubeMQ.SDK.csharp.Unified;
+using KubeMQ.SDK.csharp.Unified.Config;
+using KubeMQ.SDK.csharp.Unified.Grpc;
+
+namespace BasicClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            
+            Connection conn = new Connection().
+                SetAddress("localhost:50001").
+                SetClientId("some-client-id");
+            Client client = new Client();
+            try
+            {
+                await client.ConnectAsync(conn, CancellationToken.None);
+                Console.WriteLine("Connected");
+                await client.CloseAsync();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            
+            
+        }
+    }
+}
