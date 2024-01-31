cec42ef46dd2338a1b9d508af412b53ae379830e
Lior Nabat <lior.nabat@gmail.com>
2024-01-31 16:21:21 +0200

    Add unified PubSub client for KubeMQ
    This commit integrates a unified PubSub client for KubeMQ that allows sending and receiving events. It ensures that the client follows publish-subscribe model with classes like EventsClient, EventReceived, EventsSubscription, etc. The code incorporates options for the client to validate and handle errors along with configuration settings for connection parameters, transport controls, and TLS security.

---
+++ KubeMQ.SDK.csharp/Unified/Grpc/CustomInterceptor.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Core;
+using Grpc.Core.Interceptors;
+using KubeMQ.SDK.csharp.Unified.Config;
+
+namespace KubeMQ.SDK.csharp.Unified.Grpc
+{
+    public class CustomInterceptor : Interceptor
+    {
+        private readonly Connection opts;
+
+        public CustomInterceptor(Connection opts)
+        {
+            this.opts = opts;
+        }
+
+        public override AsyncUnaryCall<TResponse> AsyncUnaryCall<TRequest, TResponse>(
+            TRequest request, ClientInterceptorContext<TRequest, TResponse> context, AsyncUnaryCallContinuation<TRequest, TResponse> continuation)        {
+            if (!string.IsNullOrEmpty(opts.AuthToken))
+            {
+                var headers = new Metadata
+                {
+                    { "authorization", opts.AuthToken }
+                };
+                context = new ClientInterceptorContext<TRequest, TResponse>(context.Method, context.Host, context.Options.WithHeaders(headers));
+            }
+            return continuation(request, context);
+        }
+
+
+        public override async Task<TResponse> ClientStreamingServerHandler<TRequest, TResponse>(
+            IAsyncStreamReader<TRequest> requestStream, ServerCallContext context, ClientStreamingServerMethod<TRequest, TResponse> continuation)
+        {
+            if (!string.IsNullOrEmpty(opts.AuthToken))
+            {
+                context.RequestHeaders.Add("authorization", opts.AuthToken);
+            }
+            return await continuation(requestStream, context);
+        }
+
+        public override async Task ServerStreamingServerHandler<TRequest, TResponse>(
+            TRequest request, IServerStreamWriter<TResponse> responseStream, ServerCallContext context, ServerStreamingServerMethod<TRequest, TResponse> continuation)
+        {
+            if (!string.IsNullOrEmpty(opts.AuthToken))
+            {
+                context.RequestHeaders.Add("authorization", opts.AuthToken);
+            }
+            await continuation(request, responseStream, context);
+        }
+
+        public override async Task DuplexStreamingServerHandler<TRequest, TResponse>(
+            IAsyncStreamReader<TRequest> requestStream, IServerStreamWriter<TResponse> responseStream, ServerCallContext context, DuplexStreamingServerMethod<TRequest, TResponse> continuation)
+        {
+            if (!string.IsNullOrEmpty(opts.AuthToken))
+            {
+                context.RequestHeaders.Add("authorization", opts.AuthToken);
+            }
+            await continuation(requestStream, responseStream, context);
+        }
+    }
+}
