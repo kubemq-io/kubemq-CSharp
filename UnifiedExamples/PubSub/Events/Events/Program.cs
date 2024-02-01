5cfdc8376f567ca682ce993c587c9572f32047d2
Lior Nabat <lior.nabat@gmail.com>
2024-02-01 09:33:05 +0200

    Refactor connection handling and error logging in KubeMQ clients
    Revamped the methods for connecting to KubeMQ Server in several files primarily by introducing a more structured error handling logic. This significantly improves the readability and robustness of connection management by eliminating unneeded try-catch blocks and replacing them with conditional checks based on the ConnectAsync result. It also enhances error logging by explicitly detailing connection failures.

--- UnifiedExamples/PubSub/Events/Events/Program.cs
+++ UnifiedExamples/PubSub/Events/Events/Program.cs
@@ -2,6 +2,7 @@
 using KubeMQ.SDK.csharp.Unified.PubSub.Events;
 using KubeMQ.SDK.csharp.Unified;
 using KubeMQ.SDK.csharp.Unified.Config;
+using KubeMQ.SDK.csharp.Unified.Results;
 
 
 namespace Events
@@ -11,14 +12,16 @@ namespace Events
         static async Task Main(string[] args)
         {
 
-            Connection conn = new Connection().SetAddress("localhost:50000").SetClientId("some-client-id");
+            Connection conn = new Connection().SetAddress("localhost:50000").SetClientId("Some-client-id");
             Client client = new Client();
-            try
+            CancellationTokenSource cts = new CancellationTokenSource();
+            ConnectAsyncResult connectResult = await client.ConnectAsync(conn, cts.Token);
+            if (!connectResult.IsSuccess)
             {
-                CancellationTokenSource cts = new CancellationTokenSource();
-                
-                await client.ConnectAsync(conn, cts.Token);
-                var subscription = new EventsSubscription()
+                Console.WriteLine($"Could not connect to KubeMQ Server, error:{connectResult.ErrorMessage}");
+                return;
+            }
+            var subscription = new EventsSubscription()
                     .SetChannel("e1")
                     .SetGroup("")
                     .SetOnReceiveEvent(receivedEvent =>
@@ -29,24 +32,24 @@ namespace Events
                     {
                         Console.WriteLine($"Error: {exception.Message}");
                     });
-                client.SubscribeToEvents(subscription, cts.Token);
-                await Task.Delay(1000);
-                Event msg = new Event().SetChannel("e1").
-                    SetBody("hello kubemq - sending an event message"u8.ToArray());
-                await client.SendEventAsync(msg);
- 
-                Console.WriteLine("Press any key to exit...");
-                Console.ReadKey();
-                
-                cts.Cancel();
-                await client.CloseAsync();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-            }
-            
-            
+              SubscribeToEventsResult subscribeResult =  client.SubscribeToEvents(subscription, cts.Token);
+              if (!subscribeResult.IsSuccess)
+              {
+                  Console.WriteLine($"Could not subscribe to KubeMQ Server, error:{subscribeResult.ErrorMessage}");
+                  return;
+              }
+              await Task.Delay(1000);
+              Event msg = new Event().SetChannel("e1").SetBody("hello kubemq - sending an event message"u8.ToArray());
+              SendEventAsyncResult sendResult=  await client.SendEventAsync(msg, cts.Token);
+              if (!sendResult.IsSuccess)
+              {
+                  Console.WriteLine($"Could not send to KubeMQ Server, error:{sendResult.ErrorMessage}");
+                  return;
+              }
+
+              Console.WriteLine("Press any key to exit...");
+              Console.ReadKey();
+              await  client.CloseAsync();
         }
     }
 }
