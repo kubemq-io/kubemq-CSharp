417d20b7c4d4300d52dc32b72aab8e2710682e7b
KubeMQ <lior.nabat@kubemq.io>
2024-02-01 09:32:26 +0200

    Add EventsStore functionality and improve client handling mechanisms
    This commit adds new event subscription classes for managing stored events. The functionality is incorporated into UI, PubSub, and various dedicated Events classes. Additionally, client connection and error handling processes have been improved and unified. The new structure uses "ActionResult" objects for better debugging, connection error handling, and operation status tracking.

---
+++ KubeMQ.SDK.csharp/Unified/Results/SendEventAsyncResult.cs
@@ -0,0 +1,9 @@
+﻿namespace KubeMQ.SDK.csharp.Unified.Results
+{
+    /// <summary>
+    /// Represents the result of sending an event asynchronously.
+    /// </summary>
+    public class SendEventAsyncResult:BaseResult
+    {
+    }
+}
---
+++ KubeMQ.SDK.csharp/Unified/Results/SendEventStoreAsyncResult.cs
@@ -0,0 +1,9 @@
+﻿namespace KubeMQ.SDK.csharp.Unified.Results
+{
+    /// <summary>
+    /// Represents the result of sending an event store asynchronously.
+    /// </summary>
+    public class SendEventStoreAsyncResult : BaseResult
+    {
+    }
+}
