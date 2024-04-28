8e83de16fea3b285b1fac091b109bc78edd83584
Lior Nabat <lior.nabat@gmail.com>
2024-04-28 18:46:44 +0300

    Revert "Add EventsStore functionality and improve client handling mechanisms"
    This reverts commit bd840b6de5a0ee242874543c329c45610d736481.

--- KubeMQ.SDK.csharp/Unified/PubSub/Events/Events.cs
+++ KubeMQ.SDK.csharp/Unified/PubSub/Events/Events.cs
@@ -9,40 +9,20 @@ using pb= KubeMQ.Grpc;
 using static KubeMQ.Grpc.kubemq;
 namespace KubeMQ.SDK.csharp.Unified.PubSub.Events
 {
-    /// Represents a client for sending events and subscribing to events.
-    /// /
-    internal class EventsClient
+    public class EventsClient
     {
-        /// <summary>
-        /// Represents an instance of the Kubemq client for sending and receiving events.
-        /// </summary>
         private readonly kubemqClient _kubemqClient;
-
-        /// <summary>
-        /// Represents the unique identifier for the client in the KubeMQ system. </summary> <remarks>
-        /// The _clientId variable is used to identify the client when sending events or subscribing to events. </remarks>
-        /// /
         private readonly string _clientId;
 
-        /// <summary>
-        /// Provides methods for sending and subscribing to events.
-        /// </summary>
         public EventsClient(kubemqClient kubemqClient, string clientId)
         {
             _kubemqClient = kubemqClient;
             _clientId = clientId;
         }
 
-        /// <summary>
-        /// Sends an event asynchronously.
-        /// </summary>
-        /// <param name="eventToSend">The event message to send.</param>
-        /// <param name="cancellationToken">The cancellation token to cancel operation.</param>
-        /// <returns>A task representing the asynchronous operation.</returns>
         public async Task SendAsync(Event eventToSend, CancellationToken cancellationToken)
         {
-            
-            var grpcEvent = eventToSend.Validate().ToKubemqEvent(_clientId);
+            var grpcEvent = eventToSend.ToKubemqEvent(_clientId);
             var result = await _kubemqClient.SendEventAsync(grpcEvent, cancellationToken: cancellationToken);
 
             if (!result.Sent)
@@ -51,15 +31,8 @@ namespace KubeMQ.SDK.csharp.Unified.PubSub.Events
             }
         }
 
-        /// <summary>
-        /// Subscribes to events based on the provided <see cref="EventsSubscription"/>.
-        /// </summary>
-        /// <param name="subscription">The <see cref="EventsSubscription"/> object containing the channel and group to subscribe to.</param>
-        /// <param name="cancellationToken">The cancellation token.</param>
-        /// <returns>A <see cref="Task"/> representing the asynchronous operation.</returns>
         public async Task SubscribeAsync(EventsSubscription subscription, CancellationToken cancellationToken)
         {
-            subscription.Validate();
             var pbRequest = new pb.Subscribe()
             {
                 SubscribeTypeData = pb.Subscribe.Types.SubscribeType.Events,
