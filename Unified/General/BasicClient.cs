a36f345b91924790babbb083a2168518f4a3e759
KubeMQ <lior.nabat@kubemq.io>
2024-04-28 18:46:44 +0300

    Revert "Remove BasicClient.cs in Unified/General"
    This reverts commit 0906b8a004c66f86e6d42c0da2338754a07a9b99.

---
+++ Unified/General/BasicClient.cs
@@ -0,0 +1,30 @@
+﻿using KubeMQ.SDK.csharp.Unified.Config;
+using KubeMQ.SDK.csharp.Unified.Grpc;
+
+namespace Unified.General
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            Connection conn = new Connection().SetAddress("localhost:50000").SetClientId("some-client-id")
+                .SetTls(new TlsConfig().SetEnabled(true).SetCertFile("./localhost.pem")
+                    .SetKeyFile("./localhost-key.pem").SetCaFile("./rootCA.pem"));
+            Transport transport = new Transport(conn);
+            try
+            {
+                await transport.InitializeAsync(CancellationToken.None);
+                Console.WriteLine("Connected");
+                await transport.CloseAsync();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+
+
+        }
+    }
+}
