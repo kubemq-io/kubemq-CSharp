4002346e2ee9d39e981518792747d5226ca32716
KubeMQ <lior.nabat@kubemq.io>
2024-04-28 18:46:41 +0300

    Revert "Revert "Refactor connection handling and error logging in KubeMQ clients""
    This reverts commit 1df1cd39689ebe1518607cc375d02614f3042a66.

--- UnifiedExamples/General/TlsClient/Program.cs
+++ UnifiedExamples/General/TlsClient/Program.cs
@@ -1,7 +1,7 @@
 ﻿using KubeMQ.SDK.csharp.Unified;
 using KubeMQ.SDK.csharp.Unified.Config;
 using KubeMQ.SDK.csharp.Unified.Grpc;
-
+using KubeMQ.SDK.csharp.Unified.Results;
 namespace TlsClient
 {
     class Program
@@ -18,18 +18,14 @@ namespace TlsClient
                     SetKeyFile("./localhost-key.pem").
                     SetCaFile("./rootCA.pem"));
             Client client = new Client();
-            try
+            ConnectAsyncResult result = await client.ConnectAsync(conn, CancellationToken.None);
+            if (!result.IsSuccess)
             {
-                await client.ConnectAsync(conn, CancellationToken.None);
-                Console.WriteLine("Connected");
-                await client.CloseAsync();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-            }
-            
-            
+                Console.WriteLine($"Could not connect to KubeMQ Server, error:{result.ErrorMessage}");
+                return;
+            }    
+            Console.WriteLine("Connected");
+            await client.CloseAsync();
         }
     }
 }
