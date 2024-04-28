1df1cd39689ebe1518607cc375d02614f3042a66
KubeMQ <lior.nabat@kubemq.io>
2024-04-28 17:59:44 +0300

    Revert "Refactor connection handling and error logging in KubeMQ clients"
    This reverts commit b06d1f4a60edf050b3e1133f2bf8d3c72cf87b23.

--- UnifiedExamples/General/BasicClient/Program.cs
+++ UnifiedExamples/General/BasicClient/Program.cs
@@ -1,7 +1,6 @@
 ﻿using KubeMQ.SDK.csharp.Unified;
 using KubeMQ.SDK.csharp.Unified.Config;
 using KubeMQ.SDK.csharp.Unified.Grpc;
-using KubeMQ.SDK.csharp.Unified.Results;
 
 namespace BasicClient
 {
@@ -11,17 +10,21 @@ namespace BasicClient
         {
             
             Connection conn = new Connection().
-                SetAddress("localhost:50000").
+                SetAddress("localhost:50001").
                 SetClientId("some-client-id");
             Client client = new Client();
-            ConnectAsyncResult result = await client.ConnectAsync(conn, CancellationToken.None);
-            if (!result.IsSuccess)
+            try
             {
-                Console.WriteLine($"Could not connect to KubeMQ Server, error:{result.ErrorMessage}");
-                return;
-            }    
-            Console.WriteLine("Connected");
-            await client.CloseAsync();
+                await client.ConnectAsync(conn, CancellationToken.None);
+                Console.WriteLine("Connected");
+                await client.CloseAsync();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            
+            
         }
     }
 }
