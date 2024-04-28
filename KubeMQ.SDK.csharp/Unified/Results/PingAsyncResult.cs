f507b5cee84d5aadb6bad09b855b19521fd21055
Lior Nabat <lior.nabat@gmail.com>
2024-04-28 18:46:37 +0300

    Revert "Revert "Add EventsStore functionality and improve client handling mechanisms""
    This reverts commit 63cca84e0bf8717842f1f84272a2a01036c038d3.

---
+++ KubeMQ.SDK.csharp/Unified/Results/PingAsyncResult.cs
@@ -0,0 +1,15 @@
+﻿using KubeMQ.SDK.csharp.Unified.Grpc;
+
+namespace KubeMQ.SDK.csharp.Unified.Results
+{
+    /// <summary>
+    /// Represents the result of a ping operation.
+    /// </summary>
+    public class PingAsyncResult : BaseResult
+    {
+        /// <summary>
+        /// Represents information about the KubeMQ server.
+        /// </summary>
+        public ServerInfo ServerInfo { get; set; }
+    }
+}
