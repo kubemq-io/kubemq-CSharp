fbd06b078459671a34089583c2648252b1640cf1
KubeMQ <lior.nabat@kubemq.io>
2024-04-28 18:46:44 +0300

    Revert "Add EventsStore functionality and improve client handling mechanisms"
    This reverts commit 417d20b7c4d4300d52dc32b72aab8e2710682e7b.

--- KubeMQ.SDK.csharp/Unified/Results/SubscribeToEventsStoreResult.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace KubeMQ.SDK.csharp.Unified.Results
-{
-    /// <summary>
-    /// Represents the result of subscribing to events store in the KubeMQ Unified SDK.
-    /// </summary>
-    public class SubscribeToEventsStoreResult:BaseResult
-    {
-    }
-}
