a334a71934d4bc7d012fae334d6f16455dd08b4a
KubeMQ <lior.nabat@kubemq.io>
2024-04-29 14:28:05 +0300

    Add result classes to KubeMQ SDK
    Implemented result classes for various operations in the KubeMQ Unified SDK. These new classes extend the BaseResult class and provide the result structure for operations like subscribing to events, connecting asynchronously, and sending events among others.

---
+++ KubeMQ.SDK.csharp/Results/CloseAsyncResult.cs
@@ -0,0 +1,9 @@
+namespace KubeMQ.SDK.csharp.Results
+{
+    /// <summary>
+    /// Represents the result of closing a connection asynchronously.
+    /// </summary>
+    public class CloseAsyncResult:BaseResult
+    {
+    }
+}
