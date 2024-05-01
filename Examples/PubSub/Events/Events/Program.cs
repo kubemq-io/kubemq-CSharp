a17b06878bcd4e05dd579b13623b617e774d9703
KubeMQ <lior.nabat@kubemq.io>
2024-05-01 13:51:07 +0300

    Add PubSub Events Program in examples
    Added two new files in the PubSub Events example directory. The files include a new project file `Events.csproj` for configuring the project. Also added a `Program.cs` which makes use of the KubeMQ SDK to demonstrate PubSub Events through a console application.

---
+++ Examples/PubSub/Events/Events/Program.cs
@@ -0,0 +1,89 @@
+﻿using System.Text;
+using KubeMQ.SDK.csharp.Common;
+using KubeMQ.SDK.csharp.PubSub.Events;
+
+using KubeMQ.SDK.csharp.Config;
+using KubeMQ.SDK.csharp.Results;
+
+
+namespace Events
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            Connection conn = new Connection().SetAddress("localhost:50000").SetClientId("Some-client-id");
+            EventsClient client = new EventsClient();
+            CancellationTokenSource cts = new CancellationTokenSource();
+            ConnectAsyncResult connectResult = await client.Connect(conn, cts.Token);
+            if (!connectResult.IsSuccess)
+            {
+                Console.WriteLine($"Could not connect to KubeMQ Server, error:{connectResult.ErrorMessage}");
+                return;
+            }
+
+            CommonAsyncResult result = await client.Create("e1");
+            if (!result.IsSuccess)
+            {
+                Console.WriteLine($"Could not create event channel, error:{result.ErrorMessage}");
+                return;
+            }
+            
+            ListPubSubAsyncResult listResult = await client.List();
+            if (!listResult.IsSuccess)
+            {
+                Console.WriteLine($"Could not list event channels, error:{listResult.ErrorMessage}");
+                return;
+            }
+            result = await client.Delete("e1");
+            if (!result.IsSuccess)
+            {
+                Console.WriteLine($"Could not delete event channel, error:{result.ErrorMessage}");
+                return;
+            }
+            
+            await Task.Delay(2000); 
+            listResult = await client.List();
+            if (!listResult.IsSuccess)
+            {
+                Console.WriteLine($"Could not list event channels, error:{listResult.ErrorMessage}");
+                return;
+            }
+            
+            foreach (var channel in listResult.Channels)
+            {
+                Console.WriteLine($"{channel}");
+            }
+            var subscription = new EventsSubscription()
+                    .SetChannel("e1")
+                    .SetGroup("")
+                    .SetOnReceiveEvent(receivedEvent =>
+                    {
+                        Console.WriteLine($"Event Received: Id:{receivedEvent.Id}, Body:{Encoding.UTF8.GetString(receivedEvent.Body)}");
+                    })
+                    .SetOnError(exception =>
+                    {
+                        Console.WriteLine($"Error: {exception.Message}");
+                    });
+              SubscribeToEventsResult subscribeResult =  client.Subscribe(subscription, cts.Token);
+              if (!subscribeResult.IsSuccess)
+              {
+                  Console.WriteLine($"Could not subscribe to KubeMQ Server, error:{subscribeResult.ErrorMessage}");
+                  return;
+              }
+              await Task.Delay(1000);
+              Event msg = new Event().SetChannel("e1").SetBody("hello kubemq - sending an event message"u8.ToArray());
+              SendEventAsyncResult sendResult=  await client.Send(msg, cts.Token);
+              if (!sendResult.IsSuccess)
+              {
+                  Console.WriteLine($"Could not send to KubeMQ Server, error:{sendResult.ErrorMessage}");
+                  return;
+              }
+
+              Console.WriteLine("Press any key to exit...");
+              Console.ReadKey();
+              await  client.Close ();
+        }
+    }
+}
