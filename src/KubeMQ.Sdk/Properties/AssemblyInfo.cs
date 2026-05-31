51877efbbad9071f39aead187fec402f6fb58488
KubeMQ <lior.nabat@kubemq.io>
2026-05-31 18:53:56 +0300

    chore: expose SDK internals to MassTransit.KubeMQ via InternalsVisibleTo
    Enables the kubemq-masstransit transport (and its tests) to access SDK internals via its local ProjectReference.
    
    Signed-off-by: KubeMQ <lior.nabat@kubemq.io>

--- src/KubeMQ.Sdk/Properties/AssemblyInfo.cs
+++ src/KubeMQ.Sdk/Properties/AssemblyInfo.cs
@@ -3,4 +3,6 @@ using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("KubeMQ.Sdk.Tests")]
 [assembly: InternalsVisibleTo("KubeMQ.Sdk.Tests.Unit")]
 [assembly: InternalsVisibleTo("KubeMQ.Sdk.Tests.Integration")]
+[assembly: InternalsVisibleTo("MassTransit.KubeMQ")]
+[assembly: InternalsVisibleTo("MassTransit.KubeMQ.Tests")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
